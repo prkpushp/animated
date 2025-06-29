ae268546914fe83b2d4637d67f6d952a42787937
Pushp Ranjan Karn <prkpushp@gmail.com>
2025-06-29 13:18:18 +0530

    Update generate_veo2_video_locally_with_animated_animals_and_music.sh

--- generate_veo2_video_locally_with_animated_animals_and_music.sh
+++ generate_veo2_video_locally_with_animated_animals_and_music.sh
@@ -6,7 +6,7 @@ LOCATION_ID="us-central1"
 API_ENDPOINT="us-central1-aiplatform.googleapis.com"
 MODEL_ID="veo-2.0-generate-001"
 STORAGE_URI="gs://helloranjan1/output/"
-AUDIO_FILE=`ls -l *.mp3| shuf | head -n 1`
+AUDIO_FILE=`ls *.mp3| shuf | head -n 1`
 LOCAL_DIR="./videos"
 
 # Create local directory if it doesn't exist
