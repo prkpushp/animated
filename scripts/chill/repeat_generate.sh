8eb0a7174fce0d2580d68d423171899eb9c09517
Pushp Ranjan Karn <prkpushp@gmail.com>
2025-08-06 00:14:52 +0530

    Update repeat_generate.sh

--- scripts/chill/repeat_generate.sh
+++ scripts/chill/repeat_generate.sh
@@ -4,18 +4,18 @@ set -e
 PROMPT_INPUT="$1"
 ARG_INPUT="$2"
 
-for i in 1 2 3; do
+for i in 900 3600 14400; do
   echo "🌀 Run #$i of 3"
 
   echo "🔹 [1/$i] Generate prompts..."
-  bash scripts/generate_prompts.sh "$PROMPT_INPUT" "$i"
+  bash chill/scripts/generate_prompts.sh "$PROMPT_INPUT" "$i"
 
   echo "🔹 [2/$i] Generate image and music..."
-  bash scripts/generate_media.sh "$i"
+  bash chill/scripts/generate_media.sh "$i"
 
   echo "🔹 [3/$i] Upload image and music..."
-  bash scripts/upload_artifacts.sh "$i"
+  bash chill/scripts/upload_artifacts.sh "$i"
 
   echo "🔹 [4/$i] Run make_video.sh with arg: $ARG_INPUT"
-  bash scripts/make_video.sh "$ARG_INPUT" "$i"
+  bash chill/scripts/make_video.sh "$i"
 done
